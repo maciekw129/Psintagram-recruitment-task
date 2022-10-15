804617360a411cc4c87dffff05168aae1761e776
Maciej Walecki <maciejwalecki99@gmail.com>
2022-10-15 02:56:41 +0200

    fixed types

--- src/app/service/dogs.service.ts
+++ src/app/service/dogs.service.ts
@@ -3,15 +3,15 @@ import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: "root"})
 export class DogsService {
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient) {}
 
-    }
+    api: string = 'https://dog.ceo/api';
 
     fetchDogs() {
-        return this.http.get<any>('https://dog.ceo/api/breeds/list/all');
+        return this.http.get<{message: string[]}>(this.api + '/breeds/list/all');
     }
     
     fetchDogImage(breed: string) {
-        return this.http.get<any>(`https://dog.ceo/api/breed/${breed}/images/random`);
+        return this.http.get<{message: string}>(this.api + `/breed/${breed}/images/random`);
     }
 }
